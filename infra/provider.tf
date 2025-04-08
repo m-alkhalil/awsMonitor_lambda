aa467acd37372a2e3c43f02a96bf927647834989
m-alkhalil <venture23acc@gmail.com>
2025-04-08 00:06:46 -0500

    update infra resource def. tflint

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,12 @@
 terraform {
+  required_version = ">= 1.11.3"
   required_providers {
     aws = {
         source = "hashicorp/aws"
         version = "~> 5.0"
+      
     }
+    
   }
  
   backend "s3" {
--- infra/vars-main.tf
+++ infra/vars-main.tf
@@ -1,12 +1,14 @@
 variable "lockingS3-region" {
   description = "locking s3 bucket region location"
+  type = string
 }
 
 variable "s3-bucket-locking-key" {
   description = "Terraform locking file in teh s3 bucket"
   default = "awsMonitor-infra-s3-backend"
+  type = string
 }
-
 variable "aws-tf-profile" {
   description = "aws configuration profile to use with terraform"
+  type = string
 }
