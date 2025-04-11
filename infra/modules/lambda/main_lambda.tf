98e20ea6af222a5e04f8a5a7b762c62fe39b759e
m-alkhalil <venture23acc@gmail.com>
2025-04-11 01:11:11 -0500

    finalize lambda module

--- infra/modules/lambda/main_lambda.tf
+++ infra/modules/lambda/main_lambda.tf
@@ -6,7 +6,7 @@
 # aws_lambda_function
 
 # 1. Trust policy for Lambda to assume the role
-data "aws_iam_policy_document" "lamnda-assume-role-doc"{
+data "aws_iam_policy_document" "lambda-assume-role-doc"{
   version ="2025-04-10"
   statement {
     effect = "Allow"
@@ -20,13 +20,13 @@ data "aws_iam_policy_document" "lamnda-assume-role-doc"{
 
 #assume_role_policy = jsonencode({ ... })  // Trust policy
 # 2. IAM role that Lambda will assume
-resource "aws_iam_role" "rule-lambda-func" {
+resource "aws_iam_role" "role-lambda-func" {
   # create the trust policy / assume the role policy
   # Who/what is allowed to assume this role?
   # It’s attached to the role itself.
   # Defines who can use the role, not what the role can do.
-  name = "rule-lambda-func"
-  assume_role_policy = data.aws_iam_policy_document.lamnda-assume-role-doc.json
+  name = "role-lambda-func"
+  assume_role_policy = data.aws_iam_policy_document.lambda-assume-role-doc.json
 }
 
 # 3. IAM permissions policy — what Lambda can do
@@ -46,20 +46,30 @@ resource "aws_iam_policy" "policy-lambda-func" {
 
 # 5. Attach the IAM policy to the Lambda's role
 resource "aws_iam_role_policy_attachment" "attach-role-policy" {
-  role = aws_iam_role.rule-lambda-func.name
+  role = aws_iam_role.role-lambda-func.name
   policy_arn = aws_iam_policy.policy-lambda-func.arn
 }
 
-resource "aws_lambda_function" "ec2-status-func" {
-    role = ""
-  function_name = "ec2-status-func"
-
-}
-
-
-data "archive_file" "py-func"{
+data "archive_file" "py-func-zip"{
   type = "zip"
-  source_file = ""
-  output_path = ""
+  source_file = "${path.module}/src/main_lambda.py"
+  output_path = "${path.module}/src/function.zip"
 
 }
+
+resource "aws_lambda_function" "ec2-status-func" {
+  function_name = "ec2-status-func"
+  role = aws_iam_role.role-lambda-func.arn
+  handler = "lambda_function.lambda_handler"
+  runtime = "Python 3.12"
+  filename = data.archive_file.py-func-zip.output_path
+  source_code_hash = data.archive_file.py-func-zip.output_base64sha256
+  timeout = 10
+}
+
+
+# resource null_resource + local-exec can be used to archive teh file, BUT
+# - the archive command is os dependent
+# - not tf native code 
+# - less fast less clean
+# - not meant for production
